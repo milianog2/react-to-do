c3922d8e4e5d7fad70ba25ad024c93a762c5faa7
Max Garcia <milianog2@gmail.com>
2019-06-12 21:58:27 -0700

    Checkpoint 3 react-to-do: Completed

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
-import React from 'react';
-
+import React, { Component } from 'react';
 import './App.css';
+import ToDo from './components/ToDo.js';
 
-function App() {
-  return (
-    <div className="App">
-      
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+       <ul>
+         <ToDo />
+         <ToDo />
+       </ul>
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/components/ToDo.js
+++ src/components/ToDo.js
@@ -8,4 +8,4 @@
    }
  }
 
- export default ToDo;
+  export default ToDo;
